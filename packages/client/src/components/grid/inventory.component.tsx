104816dbcafcea21ccd9bfb69b11082372cc0af7
Ali Rashed <alitarekrashed@gmail.com>
2024-02-05 16:47:59 -0600

    get inventory from backend

--- packages/client/src/components/grid/inventory.component.tsx
+++ packages/client/src/components/grid/inventory.component.tsx
@@ -1,38 +1,18 @@
 import { Sheet, Table } from "@mui/joy"
 import PriceDisplay from "./cells/price-display.component"
 import { Equipment } from "@pf2e-inventory/shared"
-
-const CANDLE: Equipment = {
-  name: "candle",
-  level: 0,
-  description: "A lit candle sheds dim light in a 10-foot radius for 8 hours.",
-  price: [{ value: 1, type: "cp" }],
-  hands: "1",
-}
-
-const TRAITORS_RING: Equipment = {
-  name: "traitor's ring",
-  level: 0,
-  description:
-    "This ring has a thick band supporting a square-cut gem that can be customized to the buyer's preference. The thickness of the band allows it to be taken to any jeweler or blacksmith to be adjusted to different hands or fingers from the original make. There is a tiny clasp at the side of the gem that, when pressed, opens the gem, revealing a small, hinged compartment. This compartment is designed to hold one dose of poison, allowing wearers to slip the contents of the ring into the food or drink of an intended target. The compartment can be closed again by gently pressing the gem back into place. Noticing the compartment requires a DC 15 Perception check for anyone inspecting the ring.",
-  access: "Member of a secret society",
-  usage: "worn",
-  price: [
-    {
-      value: 1,
-      type: "gp",
-    },
-    {
-      value: 5,
-      type: "sp",
-    },
-  ],
-  rarity: "uncommon",
-}
-
-const rows: Equipment[] = [CANDLE, TRAITORS_RING]
+import { useEffect, useState } from "react"
+import { getInventory } from "../../services/inventory.service"
 
 export default function Inventory() {
+  const [inventory, setInventory] = useState<Equipment[]>([])
+
+  useEffect(() => {
+    getInventory().then((value) => {
+      setInventory(value)
+    })
+  }, [])
+
   return (
     <>
       <Sheet>
@@ -47,7 +27,7 @@ export default function Inventory() {
             </tr>
           </thead>
           <tbody>
-            {rows.map((row) => (
+            {inventory.map((row) => (
               <tr key={row.name}>
                 <td>{row.name}</td>
                 <td>{row.level}</td>
--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -7,6 +7,17 @@ app.use(express.json())
 
 const port = 3000
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+
+  res.header("Access-Control-Allow-Headers", "origin,X-Requested-With,Content-Type,Accept,Authorization")
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST PUT")
+    return res.status(200).json({})
+  }
+  next()
+})
+
 app.use("/", routes)
 
 app.listen(port, () => {
