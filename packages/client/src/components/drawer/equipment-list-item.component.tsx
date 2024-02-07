2f4a638d0ddf789056436bddb18fac1bcc1d6efa
Ali Rashed <alitarekrashed@gmail.com>
2024-02-07 11:00:27 -0600

    expands to show description

--- packages/client/src/components/drawer/equipment-list-item.component.tsx
+++ packages/client/src/components/drawer/equipment-list-item.component.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, IconButton, ListItem, ListItemText } from "@mui/material"
+import { Box, Button, Collapse, Divider, IconButton, ListItem, ListItemText } from "@mui/material"
 import { Equipment } from "@pf2e-inventory/shared"
 import { FaChevronDown, FaChevronUp, FaPlus } from "react-icons/fa"
 import { addItem } from "../../services/inventory.service"
@@ -8,14 +8,26 @@ export default function EquipmentListItem({ value }: { value: Equipment }) {
   const [open, setOpen] = useState<boolean>(false)
 
   return (
-    <ListItem divider={true} secondaryAction={<Button onClick={() => addItem(value)}>Add</Button>}>
-      <ListItemText
-        primary={
-          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-            {open ? <FaChevronUp /> : <FaChevronDown />} {value.name}
-          </Box>
+    <>
+      <ListItem
+        sx={{ cursor: "pointer" }}
+        onClick={() => setOpen(!open)}
+        secondaryAction={
+          <IconButton aria-label="add" onClick={() => addItem(value)}>
+            <FaPlus size={12} />
+          </IconButton>
         }
-      />
-    </ListItem>
+      >
+        <ListItemText
+          primary={
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              {open ? <FaChevronUp /> : <FaChevronDown />} {value.name}
+            </Box>
+          }
+        />
+      </ListItem>
+      <Collapse in={open}>{value.description}</Collapse>
+      <Divider />
+    </>
   )
 }
