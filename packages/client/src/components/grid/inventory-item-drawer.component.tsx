16494128c0e1f9f681f103ba6f4f9589ba4c22e6
Ali Rashed <alitarekrashed@gmail.com>
2024-02-07 17:19:02 -0600

    starting to structure card

--- packages/client/src/components/grid/inventory-item-drawer.component.tsx
+++ packages/client/src/components/grid/inventory-item-drawer.component.tsx
@@ -1,4 +1,4 @@
-import { Button, IconButton } from "@mui/material"
+import { Button, Card, CardContent, CardHeader, IconButton, Paper, Typography } from "@mui/material"
 import Box from "@mui/material/Box"
 import Drawer from "@mui/material/Drawer"
 import { CharacterItem } from "@pf2e-inventory/shared"
@@ -22,17 +22,31 @@ export default function InventoryItemDrawer({
   return (
     <React.Fragment>
       <Drawer anchor={anchor} open={isOpen} onClose={onClose}>
-        <Box sx={{ width: 350, height: "100%", p: 2, display: "flex", flexDirection: "column" }} role="presentation">
-          <Box sx={{ flex: "1 1 auto" }}>{value.item.description}</Box>
-          <Button
-            sx={{ width: "min-content", flex: "0 0 auto" }}
-            onClick={() => {
-              deleteItem(value.id)
-              onClose()
-            }}
-          >
-            Remove
-          </Button>
+        <Box sx={{ width: 350, height: "100%", display: "flex", flexDirection: "column" }} role="presentation">
+          <Box sx={{ flex: "1 1 auto", p: 2 }}>
+            <Card>
+              <CardContent>
+                <Typography variant="h5" gutterBottom>
+                  {value.item.name}
+                </Typography>
+                <Typography sx={{ fontSize: 12 }} color="text.secondary">
+                  Level: {value.item.level}
+                </Typography>
+                <Typography sx={{ fontSize: 14 }}>{value.item.description}</Typography>
+              </CardContent>
+            </Card>
+          </Box>
+          <Paper elevation={3} sx={{ position: "absolute", bottom: 0, width: "100%" }}>
+            <Button
+              sx={{ width: "min-content", pb: "10" }}
+              onClick={() => {
+                deleteItem(value.id)
+                onClose()
+              }}
+            >
+              Remove
+            </Button>
+          </Paper>
         </Box>
       </Drawer>
     </React.Fragment>
