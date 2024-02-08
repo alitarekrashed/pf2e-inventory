78d54e9957aa518c315b4c00a54fd5dea52a6255
Ali Rashed <alitarekrashed@gmail.com>
2024-02-08 13:47:51 -0600

    cleanup routes

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,14 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./app.tsx"
-import "./index.css"
+import Root from "./routes/root.route.tsx"
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import InventoryRoute, { loader as inventoryLoader } from "./routes/inventory.route.tsx"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Root />,
     children: [
       {
         path: "/inventory/:inventoryId",
--- packages/client/src/routes/root.route.tsx
+++ packages/client/src/routes/root.route.tsx
@@ -1,15 +1,14 @@
-import "./App.css"
 import "@fontsource/roboto/300.css"
 import "@fontsource/roboto/400.css"
 import "@fontsource/roboto/500.css"
 import "@fontsource/roboto/700.css"
 
-import DarkModeToggle, { ColorModeContext } from "./components/dark-mode-toggle.component"
+import DarkModeToggle, { ColorModeContext } from "../components/dark-mode-toggle.component"
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 import React from "react"
 import { Outlet } from "react-router-dom"
 
-function App() {
+export default function Root() {
   const [mode, setMode] = React.useState<"light" | "dark">("dark")
   const colorMode = React.useMemo(
     () => ({
@@ -46,5 +45,3 @@ function App() {
     </>
   )
 }
-
-export default App
